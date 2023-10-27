b0cefa386926b53309bfde1ae0fea2de440d9233
ayatt14 <ayatsido14@gmail.com>
2023-10-27 22:33:48 +0300

    Thurya_Al_Dj_AND_Nour_Abd_Al_Hak

--- Binary_Search_Homework/BinarySearchHomework.cs
+++ Binary_Search_Homework/BinarySearchHomework.cs
@@ -14,6 +14,32 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                                 // من الممكن تمرير قيمة 0 لتجاهلهم
         {
             //TODO: write code for the ternary search algorithm and return the index of the element
+
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int mid1 = left + (right - left) / 3;
+                int mid2 = left + 2 * (right - left) / 3;
+
+                if (arr[mid1] == key)
+                    return mid1;
+                if (arr[mid2] == key)
+                    return mid2;
+
+                if (key < arr[mid1])
+                    right = mid1 - 1;
+                else if (key > arr[mid2])
+                    left = mid2 + 1;
+                else
+                {
+                    left = mid1 + 1;
+                    right = mid2 - 1;
+                }
+            }
+
+
             return -1;
         }
 
@@ -24,14 +50,45 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                     // من الممكن تمرير قيمة 0 لتجاهلهم
         {
             //TODO: this methods is for getting the first accurence of the key and the last accurance
-            return -1;
+            start = 0;
+            end = arr.Length - 1;
+            int count = 0;
+
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+
+                if (arr[mid] == key)
+                {
+                    count++;
+                }
+
+                if (arr[mid] < key)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+
+            return count;
         }
 
         public static int GetRepeatCount(int[] arr, int key) // don't edit this line!!!
         {
             //TODO: write code to calculate the repeat count of a spacific element
             // make sure to use the previous method in this method
-            return -1;
+
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Equals(key))
+                    count++;
+            }
+            return count;
         }
+
     }
 }
